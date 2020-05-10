ab01ddaf478ffeb0386411a31cfcda24cf87aa15
avimehenwal <avi.mehanwal@gmail.com>
2020-05-11 00:36:39 +0200

    feat(component): :sparkles: Add wasm for dot language component support

--- content/.vuepress/components/Dot.vue
+++ content/.vuepress/components/Dot.vue
@@ -1,28 +1,26 @@
 <template>
   <div>
-    <h3>Dot component</h3>
+    <h3>from Component</h3>
     <div id="graph"></div>
   </div>
 </template>
 
 <script>
-import 'd3-graphviz'
-import * as d3 from 'd3'
-// https://github.com/sorieux/easyflowchart/blob/master/src/components/GraphvizViewer.vue
+import * as d3 from 'd3';
+import 'd3-graphviz';
 
 export default {
-  mounted () {
-    console.log(d3.select("#graph").graphviz().renderDot('digraph  {a -> b}'))
-    d3.select("#graph").graphviz()
-    .renderDot('digraph  {a -> b}')
-  },
-  data() {
-    return {
-      code: '',
-      width: null,
-      height: null,
-    }
+  props: [
+    'code'
+  ],
+  // data() {
+  //   return {
+  //     code: 'digraph {a -> b}'
+  //   };
+  // },
+  mounted() {
+    console.log(d3.select('#graph').graphviz("#graph").renderDot(this.code))
+    d3.select('#graph').graphviz("#graph").renderDot(this.code);
   }
-}
-// index.es6.js?5265:150 wasm streaming compile failed: TypeError: Failed to execute 'compile' on 'WebAssembly': Incorrect response MIME type. Expected 'application/wasm'.
+};
 </script>
